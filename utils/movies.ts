1e01c378a46c153305e6466bdc6d73f8992ccc1f
Stanley Nduaguibe <snduaguibe@atbtechsoft.com>
2021-11-08 19:25:50 +0100

    fifth

--- utils/movies.ts
+++ utils/movies.ts
@@ -5,34 +5,38 @@ import { logger } from "./winston";
 const { STAR_URL } = process.env;
 
 async function getMovies() {
- const { data } = await axios.get(STAR_URL);
+ try {
+  const { data } = await axios.get(STAR_URL);
 
- if (data) {
-  const moviesList = [];
-  for (const results of data.results) {
-   const { title, opening_crawl, episode_id, release_date, characters } = results;
-   const characterInfo = [];
-   for (const character of characters) {
-    try {
-     characterInfo.push((await axios.get(character)).data);
-    } catch (e) {
-     logger.error(e);
+  if (data) {
+   const moviesList = [];
+   for (const results of data.results) {
+    const { title, opening_crawl, episode_id, release_date, characters } = results;
+    const characterInfo = [];
+    for (const character of characters) {
+     try {
+      characterInfo.push((await axios.get(character)).data);
+     } catch (e) {
+      logger.error(e);
+     }
     }
+
+    moviesList.push({ title, opening_crawl, episode_id, release_date, characters: characterInfo });
    }
 
-   moviesList.push({ title, opening_crawl, episode_id, release_date, characters: characterInfo });
+   const movies = {
+    ids: moviesList.map((value) => value.episode_id),
+    movies: moviesList,
+    unfilteredMovies: data.results,
+    count: data.count,
+   };
+   globalThis.movies = movies;
   }
 
-  const movies = {
-   ids: moviesList.map((value) => value.episode_id),
-   movies: moviesList,
-   unfilteredMovies: data.results,
-   count: data.count,
-  };
-  globalThis.movies = movies;
+  logger.info("server ready for request");
+ } catch (e) {
+  logger.error(e);
  }
-
- logger.info("server ready for request");
 }
 
 getMovies();

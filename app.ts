12f2e7d2defff82b903687595489f7eed79535f2
Stanley Nduaguibe <snduaguibe@atbtechsoft.com>
2021-11-08 22:33:28 +0100

    hello

--- app.ts
+++ app.ts
@@ -1,26 +1,38 @@
+// import express from "express";
+
+// import cors from "cors";
+// import helmet from "helmet";
+// //import "./src/databaseEngine/index";
+// //import "./utils/movies";
+// //import { useMorgan } from "./utils/morgan";
+// //import { router } from "./src/routes/index";
+
+// //import { logger } from "./utils/winston";
+
+// const { PORT } = process.env;
+// const app = express();
+
+// app.use(cors());
+// app.use(helmet());
+// app.use(express.json());
+// //app.use(useMorgan);
+
+// app.get("/", function (req, res) {
+//  console.log("hello");
+// });
+
+// //app.use("/", router);
+
+// app.listen(PORT || 3000, () => console.log(`port running for user service at ${PORT}`));
+
 import express from "express";
 
 import cors from "cors";
-import helmet from "helmet";
-//import "./src/databaseEngine/index";
-//import "./utils/movies";
-//import { useMorgan } from "./utils/morgan";
-import { router } from "./src/routes/index";
 
-//import { logger } from "./utils/winston";
-
-const { PORT } = process.env;
 const app = express();
 
 app.use(cors());
-app.use(helmet());
-app.use(express.json());
-//app.use(useMorgan);
 
-app.get("/", function (req, res) {
- console.log("hello");
-});
+app.get("/", (req, res) => res.send("hello"));
 
-//app.use("/", router);
-
-app.listen(PORT || 3000, () => console.log(`port running for user service at ${PORT}`));
+app.listen(process.env.PORT || 5000, () => console.log("listening"));
